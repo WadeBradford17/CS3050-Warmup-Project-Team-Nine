3f996ad42adf4082215f679a17231fb6b0a7f857
WadeBradford17 <wadebradford179@gmail.com>
2024-09-12 13:07:15 -0400

    Added functionality to upload movie documents to firebase collection

--- firebase_admin_program.py
+++ firebase_admin_program.py
@@ -7,7 +7,7 @@ from firebase_admin import firestore, credentials
 #    pip install google-cloud-firestore
 
 # firebase admin sdk configuration
-cred = credentials.Certificate('CS3050-Warmup-Project-Team-Nine/service_account_info.json')
+cred = credentials.Certificate('service_account_info.json')
 firebase_admin.initialize_app(cred)
 
 # initialize firestore db
@@ -16,3 +16,22 @@ movie_database = firestore.client()
 # load movie data json
 with open('imdb-movie-data.json') as file:
     movies = json.load(file)
+
+# reference to collection in Firebase
+collection_ref = movie_database.collection('Movies')
+
+# create batch
+batch = movie_database.batch()
+
+for movie in movies:
+    # 'uuid' field as document ID
+    doc_ref = collection_ref.document(movie['uuid'])
+    # add document to batch
+    batch.set(doc_ref, movie)
+
+# upload movie documents
+try:
+    batch.commit()
+    print(f"All movies have been added to the Firestore Movies Collection.")
+except Exception as e:
+    print(f"Error occurred during batch commit: {e}")
