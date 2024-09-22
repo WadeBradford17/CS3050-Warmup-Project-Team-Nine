3c741f7440f34972e282805a57f61777be3e84a5
camk503 <kirn.cameron@gmail.com>
2024-09-22 15:35:58 -0400

    Changed functionality to be returning lists of strings instead of strings

--- interface.py
+++ interface.py
@@ -12,28 +12,34 @@ def load_movies():
 
 def get_title_from_firestore(field, op, value):
     docs = (movie_database.collection("Movies").where(filter=FieldFilter(field, op, value)).stream())
-    if docs is not None:
-        rtn_str = ""
-        for doc in docs:
-            rtn_str += doc.to_dict().get('title') + "\n"
-        return rtn_str
-    else:
-        return f"Could not find {value} in database"
+    titles = []
+    for doc in docs:
+        titles.append(doc.to_dict().get('title'))
+    if titles == []:
+        titles.append(f"Could not find '{value}' in database")
+    return titles
 
 
 def get_field_from_firestore(field, title):
     docs = (movie_database.collection("Movies").where(filter=FieldFilter('title', '==', title)).stream())
-    if docs is not None:
-        for doc in docs:
-            return doc.to_dict().get(field)
-    else:
-        return f"Could not find {title} in database"
+    fields = []
+    for doc in docs:
+        fields.append(doc.to_dict().get(field))
+    if fields == []:
+        fields.append(f"Could not find '{title}' in database")
+    return fields
 
 
 def get_info_from_firestore(title):
     docs = (movie_database.collection("Movies").where(filter=FieldFilter('title', '==', title)).stream())
-    if docs is not None:
-        for doc in docs:
-            return f"Rank: {doc.to_dict().get('rank')}\nYear: {doc.to_dict().get('year')}\nRating: {doc.to_dict().get('rating')}\nDuration: {doc.to_dict().get('duration')}\nGenre: {doc.to_dict().get('genre')}\nDirector: {doc.to_dict().get('director')}"
-    else:
-        return f"Could not find {title} in database"
+    movie_info = []
+    for doc in docs:
+        movie_info.append(f"Rank: {doc.to_dict().get('rank')}")
+        movie_info.append(f"Year: {doc.to_dict().get('year')}")
+        movie_info.append(f"Rating: {doc.to_dict().get('rating')}")
+        movie_info.append(f"Duration: {doc.to_dict().get('duration')}")
+        movie_info.append(f"Genre: {doc.to_dict().get('genre')}")
+        movie_info.append(f"Director: {doc.to_dict().get('director')}")
+    if movie_info == []:
+        movie_info.append(f"Could not find '{title}' in database")
+    return movie_info
