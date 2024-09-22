74cc7831738652e64976c9d99322649cb16b3d57
camk503 <kirn.cameron@gmail.com>
2024-09-22 15:41:00 -0400

    Now return lists of strings instead of strings, implemented compound queries

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import re
+
 from interface import get_title_from_firestore, get_field_from_firestore, get_info_from_firestore, load_movies
 
 fields = ['title', 'rating', 'year', 'duration', 'director', 'genre', 'viewers', 'rank']
@@ -6,10 +8,12 @@ joiners = ['||', '&&']
 keywords = ['help', 'quit']
 valid_eq_check = ['title', 'director', 'genre', 'viewers']
 valid_comp_check = ['year', 'rank', 'rating', 'duration']
-valid_belonging_check = ['rating', 'year', 'duration', 'director', 'genre', 'viewers', 'rank']
+valid_belonging_check = fields
 eq_operators = ['==', '!=']
 comp_operators = ['==', '>', '<', '>=', '<=', '!=']
 movies = load_movies()
+
+INVALID_QUERY = ["Invalid Query"]
 
 
 def is_valid_num(num):
@@ -20,69 +24,114 @@ def is_valid_num(num):
 
 def get_user_input(input):
     if '"' not in input or input.index('"') == input.rfind('"'):
-        return "Invalid Query"
+        return INVALID_QUERY
     return input[input.index('"') + 1:input.rfind('"')]
 
+
+def input_not_found(list):
+    if len(list) == 1 and re.fullmatch(r"Could not find '.*' in database", list[0]):
+        return True
+    return False
+
+def help_menu():
+    menu = ['Queryable column keywords: title, rating, year, duration, director, genre, viewers, rank',
+            'Query operators: ==, >, >=, <, <=, !=, of, all',
+            'Query joiners: &&, ||',
+            'Query Structure: keyword operator "input"',
+            'Compound Query: keyword operator "input" joiner keyword operator "input"',
+            'Note: The query input is case sensitive and the inputs must be in double quotes',
+            'Example query:',
+            '> director of "Star Wars"',
+            'George Lucas']
+    return menu
 
 def parse_query(query):
     if query in keywords:
         if query == 'help':
-            return "Help Menu"
+            return help_menu()
         else:
             exit(0)
 
     for joiner in joiners:
         if f' {joiner} ' in query:
             sliceIndex = query.index(f' {joiner} ')
-            if (sliceIndex + 4 > len(query)):
-                return "Invalid Query"
+            if sliceIndex + 4 > len(query):
+                return INVALID_QUERY
             firstQuery = query[0:sliceIndex]
             secondQuery = query[sliceIndex + 4:len(query)]
             if firstQuery not in keywords and secondQuery not in keywords:
-                if parse_query(firstQuery) != "Invalid Query" and parse_query(secondQuery) != "Invalid Query":
+                if parse_query(firstQuery) != INVALID_QUERY and parse_query(secondQuery) != INVALID_QUERY:
                     if ' of ' not in firstQuery and ' of ' not in secondQuery:
-                        return parse_query(firstQuery) + f' {joiner} ' + parse_query(secondQuery)
+                        first_return = parse_query(firstQuery)
+                        second_return = parse_query(secondQuery)
+                        if input_not_found(first_return):
+                            return first_return
+                        if input_not_found(second_return):
+                            return second_return
+                        compound_return = []
+                        if joiner == '&&':
+                            for item in first_return:
+                                if item in second_return:
+                                    compound_return.append(item)
+                        else:
+                            for item in first_return:
+                                if item not in compound_return:
+                                    compound_return.append(item)
+                            for item in second_return:
+                                if item not in compound_return:
+                                    compound_return.append(item)
+                        return compound_return
                     else:
-                        return "Can't conjoin an 'of' check"
+                        return ["Can't conjoin an 'of' check"]
                 else:
-                    return "Invalid Query"
+                    return INVALID_QUERY
             else:
-                return "Invalid Query"
+                return INVALID_QUERY
 
     for operator in operators:
         if f' {operator} ' in query:
             sliceIndex = query.index(f' {operator} ')
             if (sliceIndex + 4 > len(query)):
-                return "Invalid Query"
+                return INVALID_QUERY
             firstTerm = query[0:sliceIndex]
             secondTerm = query[sliceIndex + 4:len(query)]
             input = get_user_input(query)
-            if input == "Invalid Query" and secondTerm != 'all':
-                return "Invalid Query"
+            if input == INVALID_QUERY:
+                if secondTerm != 'all' or operator != 'of':
+                    return INVALID_QUERY
             if firstTerm in valid_eq_check and operator in eq_operators:
-                if firstTerm != 'title':
-                    return get_title_from_firestore(firstTerm, operator, input)
-                else:
-                    return get_info_from_firestore(input)
+                return get_title_from_firestore(firstTerm, operator, input)
             elif firstTerm in valid_comp_check and operator in comp_operators:
                 if not is_valid_num(input):
-                    return "Invalid Query"
+                    return INVALID_QUERY
                 return get_title_from_firestore(firstTerm, operator, float(input))
             elif firstTerm in valid_belonging_check and operator == 'of':
+                if firstTerm == 'title' and secondTerm != 'all':
+                    return INVALID_QUERY
                 if secondTerm == 'all':
-                    rtn_str = ""
+                    field_list = []
                     for movie in movies:
-                        rtn_str += get_field_from_firestore(firstTerm, movie)
-                    return rtn_str
+                        rtn_list = get_field_from_firestore(firstTerm, movie)
+                        for item in rtn_list:
+                            if item not in field_list:
+                                field_list.append(item)
+                    return field_list
                 else:
                     return get_field_from_firestore(firstTerm, input)
 
-    return "Invalid Query"
+    return INVALID_QUERY
 
 
 if __name__ == '__main__':
     contQueries = True
-    while(contQueries):
+    while contQueries:
         query = input("> ")
-        print(parse_query(query))
+        rtn_list = parse_query(query)
+        rtn_str = ""
+        for item in rtn_list:
+            rtn_str += item + "\n"
+        if (rtn_str != ""):
+            print(rtn_str.rstrip('\n'))
+        else:
+            print("N/A")
     exit(0)
